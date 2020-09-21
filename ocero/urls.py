6e4cb635455c5306b299b3d1e60873e73349d50d
Ryo OIKAWA <r_oikawa@dpt-inc.co.jp>
2020-09-21 15:55:20 +0900

    oserofin

--- ocero/urls.py
+++ ocero/urls.py
@@ -6,4 +6,5 @@ app_name = 'ocero'
 urlpatterns = [
     path('',views.index,name = 'index'),#通常
     path('change/',views.change,name = 'change'),#通常
+    path('onceindex/',views.onceindex,name = 'onceindex'),
 ]
--- ocero/views.py
+++ ocero/views.py
@@ -2,53 +2,140 @@ from django.shortcuts import render
 
 from django.http import request,HttpResponseRedirect
 from django.urls import reverse
-#ToDo
 
-#グリッド数8を取得
-#その後...
 
 class colt():
     def __init__(self,x,y):
+        self.state = 0
         self.x = x
         self.y = y
-        self.state = 0
 
     def flip(self):
         self.state = self.state*(-1)
 
     def put(self,turn):
         self.state = turn
-        
-field = []
-for i in range(0,8):
-    for j in range(0,8):
-        field.append(colt(i,j))
+
+field =[[0 for j in range(8)] for i in range(8)]
+turncount = 0
+judge = 0
+
+def init():
+    for k in range(0,8):
+        for l in range(0,8):
+            field[l][k] = colt(l,k)
+            if k==3 and l==3 or k==4 and l==4:
+                field[l][k].put(1)
+            if k==3 and l==4 or k==4 and l==3:
+                field[l][k].put(-1)
+
+init()
 
 # Create your views here.
 
 
 def index(request):
-
-    context = {'field':field,}
+    global turncount
+    global judge
+    turncount+=1
+    explore()
+    black = countBW(1)
+    white = countBW(-1)
+    putbool = putjudge()
+    if black+white==64 or black==0 or white==0:
+        judge = 2
+    context = {'field':field,'black':black,'white':white,'putbool':putbool,'judge':judge}
     return render(request,'ocero/index.html',context)
 
-def change(request):
-    if request.method =='POST':
-        for i in range(0,4):
-            for j in range(0,4):
+def putjudge():
+    for i in range(0,8):
+        for j in range(0,8):
+            if field[j][i].state==2:
+                return False
+    global judge
+    judge+=1
+    return True
 
-                if str(i)+ ' '+ str(j) in request.POST:
-                    flick(i,j)
-    
+def onceindex(request):
     return HttpResponseRedirect(reverse('ocero:index'))
 
 
-def flick(i,j):
-    for col in field:
-        if col.x == i and col.y == j:
-            col.put(1)
+def change(request):
+    global turncount
+    if request.method =='POST':
+        for i in range(0,8):
+            for j in range(0,8):
+                if str(j)+ ' '+ str(i) in request.POST:
+                    if field[j][i].state == 2:
+                        global judge
+                        judge=0
+                        putColt(j,i)
+                        flick(j,i,True)
+                    else:
+                        turncount+=1
+    
+    return HttpResponseRedirect(reverse('ocero:index'))
+
+def putColt(x,y):
+    turn = count()
+    field[x][y].put(turn)
+
+def deleteColt(x,y):
+    field[x][y].put(0)
+
+def countBW(bwcolt):
+    coltcount = 0
+    for i in range(0,8):
+        for j in range(0,8):
+            if field[j][i].state==bwcolt:
+                coltcount += 1
+    return coltcount
+
+def count():
+    #total = countBW(1) + countBW(-1)
+    if turncount%2==0:
+        turn = 1
+    else:
+        turn = -1
+    return turn
+
+def explore():
+    for i in range(0,8):
+        for j in range(0,8):
+            if field[j][i].state == 0 or field[j][i].state == 2:
+                putColt(j,i)
+                flick(j,i,False)
+                if field[j][i].state != 2:
+                    deleteColt(j,i)
+
+def flick(x,y,mode):
+    for i in range(-1,2):
+        for j in range(-1,2):
+            if i != 0 or j != 0:
+                changeColt(x,y,j,i,mode)
 
 
+def changeColt(x,y,xarrow,yarrow,mode):
+    if x+xarrow>=0 and x+xarrow<=7 and y+yarrow>=0 and y+yarrow<=7:
+        next = field[x+xarrow][y+yarrow].state
+        if next*(-1) == field[x][y].state:
+            for i in range(2,8):
+                if xarrow<0 and x-i<0:
+                    break
+                elif xarrow>0 and x+i>7:
+                    break
+                if yarrow<0 and y-i<0:
+                    break
+                elif yarrow>0 and y+i>7:
+                    break
+                if field[x+i*xarrow][y+i*yarrow].state == 0:
+                    break
+                if field[x+i*xarrow][y+i*yarrow].state == field[x][y].state:
+                    if mode == True:
+                        for j in range(1,i):
+                            field[x+j*xarrow][y+j*yarrow].state = field[x][y].state
+                    else:
+                        field[x][y].state = 2
 
 
 
